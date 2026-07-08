174a4e3915af933c2437664fe4faffa0e01c393f
tetrashop <ramin.edjlal1359@gmail.com>
2026-07-08 15:55:47 +0330

    Add Android WebView app and GitHub Actions for automatic APK builds on tags

---
+++ android/app/src/main/java/com/refrigitz/app/MainActivity.java
@@ -0,0 +1,23 @@
+package com.refrigitz.app;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(false);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        webView.loadUrl("https://refrigitz-api.vercel.app");
+        setContentView(webView);
+    }
+}
